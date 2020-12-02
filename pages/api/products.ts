2e8e98e2b5e5566c7b3a3e8fae4cdf63d5323dce
Zoe Heins <zoe.heins@thinkful.com>
2020-12-02 13:29:21 -0800

    lint

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 
-const { MONGODB_URI, MONGODB_DB } = process.env
+const { MONGODB_URI, MONGODB_DB } = process.env;
 
 // Create cached connection variable
-let cachedDb = null
+let cachedDb = null;
 
 // A function for connecting to MongoDB,
 // taking a single parameter of the connection string
@@ -11,23 +11,26 @@ async function connectToDatabase() {
   // If the database connection is cached,
   // use it instead of creating a new connection
   if (cachedDb) {
-    return cachedDb
+    return cachedDb;
   }
 
   // If no connection is cached, create a new one
-  const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  const client = await MongoClient.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   // Select the database through the connection,
   // using the database path of the connection string
-  const db = await client.db(MONGODB_DB)
+  const db = await client.db(MONGODB_DB);
 
   // Cache the database connection and return the connection
-  cachedDb = db
-  return db
+  cachedDb = db;
+  return db;
 }
 
 const handler = async (req, res) => {
-  const db = await connectToDatabase()
+  const db = await connectToDatabase();
   const collection = await db.collection('products');
   const products = await collection.find({}).toArray();
 
