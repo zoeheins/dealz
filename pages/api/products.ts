379d38143de27cf26c172c283292e7c3d0a40f1f
Zoe Heins <zoe.heins@thinkful.com>
2020-12-03 08:24:36 -0800

    move database conn to own file

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,33 +1,4 @@
-import { MongoClient } from 'mongodb';
-
-const { MONGODB_URI, MONGODB_DB } = process.env;
-
-// Create cached connection variable
-let cachedDb = null;
-
-// A function for connecting to MongoDB,
-// taking a single parameter of the connection string
-async function connectToDatabase() {
-  // If the database connection is cached,
-  // use it instead of creating a new connection
-  if (cachedDb) {
-    return cachedDb;
-  }
-
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  // Select the database through the connection,
-  // using the database path of the connection string
-  const db = await client.db(MONGODB_DB);
-
-  // Cache the database connection and return the connection
-  cachedDb = db;
-  return db;
-}
+import connectToDatabase from '../connectToDatabase';
 
 const handler = async (req, res) => {
   const db = await connectToDatabase();
---
+++ pages/connectToDatabase.ts
@@ -0,0 +1,33 @@
+import { connect } from 'http2';
+import { MongoClient } from 'mongodb';
+
+const { MONGODB_URI, MONGODB_DB } = process.env;
+
+// Create cached connection variable
+let cachedDb = null;
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+const connectToDatabase = async () => {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(MONGODB_DB);
+
+  // Cache the database connection and return the connection
+  cachedDb = db;
+  return db;
+};
+
+export default connectToDatabase;
