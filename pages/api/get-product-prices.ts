08af0792cea2303396f8e67e22eec24655efae1a
Zoe Heins <zoe.heins@thinkful.com>
2020-12-07 13:35:01 -0800

    check secret header from cron job

--- pages/api/get-product-prices.ts
+++ pages/api/get-product-prices.ts
@@ -4,8 +4,15 @@ import getProductPrices from '~/services/getProductPrices';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await getProductPrices();
-    res.send(200);
+    const headers = req.headers;
+    const secretHeader = headers['secret-header'];
+
+    if (secretHeader !== process.env.WEBHOOK_SECRET) {
+      res.send(401);
+    } else {
+      await getProductPrices();
+      res.send(200);
+    }
   } catch (err) {
     console.log('Error getting products prices:', err);
     res.send(500);
