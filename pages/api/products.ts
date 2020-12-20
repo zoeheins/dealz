72dbd11bc8f5eafc1e8c79f1d4bb753c2a9e5484
Zoe Heins <zoe.heins@thinkful.com>
2020-12-20 12:05:00 -0800

    add logs

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,16 +4,18 @@ import connectToDatabase from '~/utils/connectToDatabase';
 import { Product } from '~/utils/types';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // try {
-  //   const db = await connectToDatabase();
-  //   const collection = await db.collection('products');
-  //   const products: Product[] = await collection.find({}).toArray();
-  //   res.status(200).json({ products });
-  // } catch (err) {
-  //   console.log('Error connecting to db:', err);
-  //   res.status(500);
-  //   res.end();
-  // }
+  try {
+    const db = await connectToDatabase();
+    const collection = await db.collection('products');
+    const products: Product[] = await collection.find({}).toArray();
+    console.log('products from db', products)
+    // res.status(200).json({ products });
+  } catch (err) {
+    console.log('Error connecting to db:', err);
+    // res.status(500);
+    // res.end();
+  }
+
   const products = [
     {
       _id: 123,
