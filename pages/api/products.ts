f3a1efbd574b74b582d65f18c218df3343bcd52f
Zoe Heins <zoe.heins@thinkful.com>
2020-12-20 12:08:39 -0800

    put back db connection

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -8,23 +8,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const db = await connectToDatabase();
     const collection = await db.collection('products');
     const products: Product[] = await collection.find({}).toArray();
-    console.log('products from db', products)
-    // res.status(200).json({ products });
+
+    console.log('products from db', products);
+
+    res.status(200).json({ products });
   } catch (err) {
     console.log('Error connecting to db:', err);
-    // res.status(500);
-    // res.end();
-  }
 
-  const products = [
-    {
-      _id: 123,
-      url: 'fake-url.com',
-      nickname: 'fake item',
-      price: 1,
-    },
-  ];
-  res.status(200).json({ products });
+    res.status(500);
+    res.end();
+  }
 };
 
 export default handler;
