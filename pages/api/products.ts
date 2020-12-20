f398ad6c7e1c6a667eae9fc5d415aae7805f3c18
Zoe Heins <zoe.heins@thinkful.com>
2020-12-20 11:57:25 -0800

    add fake data

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,16 +4,25 @@ import connectToDatabase from '~/utils/connectToDatabase';
 import { Product } from '~/utils/types';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const db = await connectToDatabase();
-    const collection = await db.collection('products');
-    const products: Product[] = await collection.find({}).toArray();
-    res.status(200).json({ products });
-  } catch (err) {
-    console.log('Error connecting to db:', err);
-    res.status(500);
-    res.end();
-  }
+  // try {
+  //   const db = await connectToDatabase();
+  //   const collection = await db.collection('products');
+  //   const products: Product[] = await collection.find({}).toArray();
+  //   res.status(200).json({ products });
+  // } catch (err) {
+  //   console.log('Error connecting to db:', err);
+  //   res.status(500);
+  //   res.end();
+  // }
+  const products = [
+    {
+      _id: 123,
+      url: 'fake-url.com',
+      nickname: 'fake item',
+      price: '$1',
+    },
+  ];
+  res.status(200).json({ products });
 };
 
 export default handler;
