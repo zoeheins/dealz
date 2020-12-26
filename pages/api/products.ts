661dfb6c383414d52c8076e6268c56dcedce2870
Zoe Heins <zoe.heins@thinkful.com>
2020-12-25 22:42:14 -0800

    insert new product to db

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -7,14 +7,12 @@ import updateProductPrices from 'services/updateProductPrices';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // await updateProductPrices()  // update db with latest prices
+
   if (req.method === 'GET') {
     try {
       const db = await connectToDatabase();
       const collection = await db.collection('products');
       const products: DBProduct[] = await collection.find({}).toArray();
-
-      // console.log('products from db', products);
-
       res.status(200).json({ products });
     } catch (err) {
       console.log('Error connecting to db:', err);
@@ -23,10 +21,25 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       res.end();
     }
   } else if (req.method === 'POST') {
-    console.log(req.body)
-    res.status(500)
-    res.end()
+    console.log('Got POST request with body:', req.body);
+    const { name, url } = JSON.parse(req.body);
 
+    if (!(name && url)) {
+      res.status(400);
+      res.end();
+    } else {
+      try {
+        const db = await connectToDatabase();
+        const collection = await db.collection('products');
+        collection.insert({ nickname: name, url });
+        res.status(200);
+        res.end();
+      } catch (err) {
+        console.log('Error adding product:', err);
+        res.status(500);
+        res.end();
+      }
+    }
   }
 };
 
